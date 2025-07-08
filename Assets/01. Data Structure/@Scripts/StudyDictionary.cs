9927c1120765d1ad5fa7149a840436127dbb8981
Seo98 <tjtmdvy8271@naver.com>
2025-07-08 14:46:14 +0900

    1
    1

--- Assets/01. Data Structure/@Scripts/StudyDictionary.cs
+++ Assets/01. Data Structure/@Scripts/StudyDictionary.cs
@@ -1,27 +1,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class PersonData
+{
+    public int age;
+    public string name;
+    public float height;
+    public float weight;
+
+    public PersonData(int age, string name, float height, float weight)
+    {
+        this.age = age;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+
 public class StudyDictionary : MonoBehaviour
 {
-    public Dictionary<string, int> persons = new Dictionary<string, int>();
+    public Dictionary<string, PersonData> persons = new Dictionary<string, PersonData>();
 
-    private void Start()
+    void Start()
     {
-        persons.Add("Ã¶¼ö", 10);
-        persons.Add("¿µÈñ", 15);
-        persons.Add("µ¿¼ö", 17);
+        persons.Add("Ã¶¼ö", new PersonData(10, "Ã¶¼ö", 150f, 30f));
+        persons.Add("¿µÈñ", new PersonData(10, "¿µÈñ", 150f, 30f));
+        persons.Add("µ¿¼ö", new PersonData(10, "µ¿¼ö", 150f, 30f));
 
-        int age = persons["Ã¶¼ö"]; // key°ªÀ¸·Î value Ãâ·Â
-        Debug.Log($"Ã¶¼öÀÇ ³ªÀÌ´Â {age}ÀÔ´Ï´Ù.");
-
-        foreach (var person in persons)
-        {
-            if(person.Value == 15)
-            {
-                Debug.Log($"³ªÀÌ°¡ 15ÀÎ »ç¶÷ÀÇ ÀÌ¸§Àº {person.Key}ÀÔ´Ï´Ù.");
-
-            }
-        }
+        Debug.Log(persons["Ã¶¼ö"].age);
+        Debug.Log(persons["Ã¶¼ö"].name);
+        Debug.Log(persons["Ã¶¼ö"].height);
+        Debug.Log(persons["Ã¶¼ö"].weight);
     }
-
 }
